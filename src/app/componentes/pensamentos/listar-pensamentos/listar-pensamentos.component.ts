39fca6298fdb7d2cff74e468488f73d1fef3bcd2
moiseslx <moisesalmeida.dev@gmail.com>
2024-10-21 09:25:07 -0400

    adionando button para carregar pensamento com http params

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -5,17 +5,28 @@ import { PensamentoService } from '../../../services/pensamento.service';
 @Component({
   selector: 'app-listar-pensamentos',
   templateUrl: './listar-pensamentos.component.html',
-  styleUrl: './listar-pensamentos.component.css'
+  styleUrl: './listar-pensamentos.component.css',
 })
-export class ListarPensamentosComponent implements OnInit{
-  listaPensamentos : Pensamento[] = [];
+export class ListarPensamentosComponent implements OnInit {
+  listaPensamentos: Pensamento[] = [];
 
-  constructor(private service: PensamentoService) { }
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
+
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.service.listar().subscribe((value) => {
-      this.listaPensamentos = value
-    }
-    )
+    this.service.listar(this.paginaAtual).subscribe((value) => {
+      this.listaPensamentos = value;
+    });
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe((value) => {
+      this.listaPensamentos.push(...value);
+      if (!value.length) {
+        this.haMaisPensamentos = false;
+      }
+    });
   }
 }
