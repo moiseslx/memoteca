56703b687a39f7c1b6268af124579b8e512a8f80
moiseslx <moisesalmeida.dev@gmail.com>
2024-10-21 10:08:01 -0400

    incrementando pesquisar com filter no http params

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -12,21 +12,30 @@ export class ListarPensamentosComponent implements OnInit {
 
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = '';
 
   constructor(private service: PensamentoService) {}
-
+  
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((value) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((value) => {
       this.listaPensamentos = value;
     });
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual).subscribe((value) => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe((value) => {
       this.listaPensamentos.push(...value);
       if (!value.length) {
         this.haMaisPensamentos = false;
       }
     });
+  }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro).subscribe(value => {
+      this.listaPensamentos = value
+    })
   }
 }
